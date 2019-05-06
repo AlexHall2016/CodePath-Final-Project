939956b7e694a2281462bcd403858fd54338f5da
Anthony Owyeong <littledude96@gmail.com>
2019-05-05 18:10:31 -0700

    added lives, game over screen, settings screen

--- Galaga-Tribute/GameOverViewController.swift
+++ Galaga-Tribute/GameOverViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class GameOverViewController: UIViewController {
 
+    @IBOutlet weak var newGameButton: UIButton!
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        homeButton.clipsToBounds = true
+        homeButton.layer.cornerRadius = 25
+        
+        newGameButton.clipsToBounds = true
+        newGameButton.layer.cornerRadius = 25
+        
         // Do any additional setup after loading the view.
     }
+    
+    
+    
+    
     
 
     /*
